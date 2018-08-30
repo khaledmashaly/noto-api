4d6d35857d3610b1c791fcd30327955b93ee0953
5aledmaged <5aledmaged@gmail.com>
2018-08-30 06:49:36 +0200

    use moran middleware for logging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
+import logger from 'morgan';
 import noteRouter from './routes/notes';
 
 const app = express();
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notes', noteRouter);
 

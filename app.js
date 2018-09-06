a492015848dc8380075db5870e012c0630afa08f
5aledmaged <5aledmaged@gmail.com>
2018-09-06 06:25:37 +0200

    use json middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notes', noteRouter);
 

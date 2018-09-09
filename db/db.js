b2ccd5086e31ba43219d58611b2744e2be11f592
5aledmaged <5aledmaged@gmail.com>
2018-09-09 19:31:10 +0200

    moved mongo stuff to a db module

---
+++ db/db.js
@@ -0,0 +1,72 @@
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb://localhost:27017/';
+const db = 'noto';
+const col = 'notes';
+const options = {
+	useNewUrlParser: true
+};
+
+export const findAll = () => {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, options)
+			.then(client => {
+				client.db(db).collection(col)
+					.find({}).toArray()
+						.then(docs => {
+							resolve(docs);
+						})
+						.catch(e => reject(e));
+				client.close();
+			})
+			.catch(e => reject(e));
+	});
+};
+
+export const findOne = doc => {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, options)
+			.then(client => {
+				client.db(db).collection(col)
+					.findOne(doc)
+					.then(doc => {
+						resolve(doc);
+					})
+					.catch(e => reject(e));
+				client.close();
+			})
+			.catch(e => reject(e));
+	});
+};
+
+export const deleteOne = doc => {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, options)
+			.then(client => {
+				client.db(db).collection(col)
+					.deleteOne(doc)
+					.then(doc => {
+						resolve(doc);
+					})
+					.catch(e => reject(e));
+				client.close();
+			})
+			.catch(e => reject(e));
+	});
+};
+
+export const updateOne = (doc, update) => {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, options)
+			.then(client => {
+				client.db(db).collection(col)
+					.updateOne(doc, update)
+					.then(doc => {
+						resolve(doc);
+					})
+					.catch(e => reject(e));
+				client.close();
+			})
+			.catch(e => reject(e));
+	});
+};
--- routes/notes.js
+++ routes/notes.js
@@ -1,100 +1,53 @@
 import { Router } from 'express';
-import { MongoClient, ObjectID } from 'mongodb';
+import { ObjectID } from 'mongodb';
 import assert from 'assert';
+import { findAll, findOne, deleteOne, updateOne } from '../db/db';
 
 const noteRouter = new Router();
-const mongoUrl = 'mongodb://localhost:27017';
 
 noteRouter.route('/')
-          .get((req, res) => {
-            MongoClient.connect(mongoUrl, (err, client) => {
-              assert.equal(null, err);
-              console.log('connection success yaaaaaaay!');
-
-              const db = client.db('noto');
-              const collection = db.collection('notes');
-
-              collection.find({})
-                        .toArray()
-                        .then((docs) => {
-                          console.log('successfully retrieved notes from server');
-                          res.status(200).json(docs);
-                        })
-                        .catch((err) => console.error(err));
-
-              client.close();
-            });
-          })
-          .post((req, res) => {
-            res.status(201).json({'hello': 'world'});
-          });
+	.get((req, res) => {
+		findAll().then(docs => {
+				res.status(200).json(docs);
+			})
+			.catch(err => console.error(err));
+	})
+	.post((req, res) => {
+		res.status(201).json({'hello': 'world'});
+	});
 
 noteRouter.route('/:id')
-          .get((req, res) => {
-            const id = new ObjectID(req.params.id);
-            MongoClient.connect(mongoUrl, (err, client) => {
-              assert.equal(null, err);
-
-              const db = client.db('noto');
-              const collection = db.collection('notes');
-
-              collection.findOne({_id: id})
-                .then((doc) => {
-                  res.status(200).json(doc);
-                })
-                .catch((err) => console.error(err));
-
-              client.close();
-            });
-          })
-          .delete((req, res) => {
-            const id = new ObjectID(req.params.id);
-            MongoClient.connect(mongoUrl, (err, client) => {
-              assert.equal(null, err);
-              console.log('connection success yaaaaaaay!');
-
-              const db = client.db('noto');
-              const collection = db.collection('notes');
-
-              console.log('id:', id);
-
-              collection.deleteOne({ _id: id })
-                .then(({ acknowledged, deletedCount }) => {
-                  assert.equal(deletedCount, 1);
-                  res.status(204).send();
-                })
-                .catch((err) => console.error(err));
-
-              client.close();
-            });
-          })
-          .put((req, res) => {
-            const id = new ObjectID(req.params.id);
-            const { title: newTitle, body: newBody } = req.body;
-            MongoClient.connect(mongoUrl, (err, client) => {
-              assert.equal(null, err);
-
-              const db = client.db('noto');
-              const collection = db.collection('notes');
-
-              collection.updateOne(
-                { _id: id },
-                {
-                  $set: {
-                    title: newTitle,
-                    body: newBody
-                  }
-                }
-              )
-                .then((doc) => {
-                  assert.equal(doc.modifiedCount, 1);
-                  console.log('successfully edited notes from server');
-                  res.status(204).send();
-                })
-                .catch((err) => console.error(err));
-
-              client.close();
-            });
-          });
+	.get((req, res) => {
+		const id = new ObjectID(req.params.id);
+		findOne({ _id: id })
+			.then(doc => {
+				res.status(200).json(doc);
+			})
+			.catch(err => console.error(err));
+	})
+	.delete((req, res) => {
+		const id = new ObjectID(req.params.id);
+		deleteOne({ _id: id })
+			.then(({ deletedCount }) => {
+				assert.equal(deletedCount, 1);
+				res.status(204).send();
+			})
+			.catch((err) => console.error(err));
+	})
+	.put((req, res) => {
+		const id = new ObjectID(req.params.id);
+		const { title: newTitle, body: newBody } = req.body;
+		updateOne({ _id: id }, {
+			$set: {
+				title: newTitle,
+				body: newBody
+			}
+		})
+		.then(({ modifiedCount }) => {
+			assert.equal(modifiedCount, 1);
+			res.status(204).send();
+		})
+		.catch((err) => console.error(err));
+	});
 
 export default noteRouter;

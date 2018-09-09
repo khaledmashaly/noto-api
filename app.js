dfe89a5ca4e4c1c3227a4a7cecb4bc1953f6e9e1
5aledmaged <5aledmaged@gmail.com>
2018-09-09 07:36:00 +0200

    install and use helmet middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import path from 'path';
 import logger from 'morgan';
 import noteRouter from './routes/notes';
@@ -6,15 +7,16 @@ import noteRouter from './routes/notes';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notes', noteRouter);
 
 app.get('*', (req, res) => {
-  res.redirect('/');
+	res.redirect('/');
 });
 
 app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+	console.log(`app running on port ${port}`);
 });

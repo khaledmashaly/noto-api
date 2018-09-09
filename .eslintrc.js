1527a7f91c408f489076f4857a6eb245c57c8074
5aledmaged <5aledmaged@gmail.com>
2018-09-09 07:38:03 +0200

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
-  },
-  "parserOptions": {
-    "sourceType": "module",
-  },
-  "extends": ["eslint:recommended", "google"],
-  "rules": {
-    "comma-dangle": 0,
-    "no-console": 1,
-    "max-len": 0,
-    "object-curly-spacing": 0
-  }
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"parserOptions": {
+		"sourceType": "module",
+	},
+	"extends": ["eslint:recommended", "google"],
+	"rules": {
+		"comma-dangle": 0,
+		"no-console": 1,
+		"max-len": 0,
+		"object-curly-spacing": 0,
+		"arrow-parens": 0,
+		"no-tabs": 0
+	}
 };

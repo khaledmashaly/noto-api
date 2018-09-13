2f2f807f66a03aa9ff6a5193bb24bebf74a29aff
5aledmaged <5aledmaged@gmail.com>
2018-09-13 18:45:50 +0200

    changed default task to run clean and build in series

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,14 +4,14 @@ import babel from 'gulp-babel';
 import rimraf from 'rimraf';
 
 const paths = {
-  styles: {
-    src: [],
-    dest: []
-  },
-  scripts: {
+	styles: {
+		src: [],
+		dest: []
+	},
+	scripts: {
 		src: ['app.js', 'routes/*.js', 'db/*.js'],
 		dest: ['dist/']
-  }
+	}
 };
 
 const srcOptions = { base: '.' };
@@ -24,13 +24,13 @@ const clean = (cb) => {
 export const buildJS = () => {
 	// TODO: uglify and source maps
 	return src(paths.scripts.src, srcOptions)
-      .pipe(eslint())
-      .pipe(eslint.format())
+		.pipe(eslint())
+		.pipe(eslint.format())
 		.pipe(eslint.failAfterError())
 		.pipe(babel())
 		.pipe(dest(paths.scripts.dest));
 };
 
-gulp.task('build', scripts);
+const build = series(clean, buildJS);
 
-export default scripts;
+export default build;

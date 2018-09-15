dd73331c1cadf0a26d88426f12a6a175fed21ea3
5aledmaged <5aledmaged@gmail.com>
2018-09-15 19:28:01 +0200

    eslint: set no-console to error on NODE_ENV=production

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
 	extends: ['eslint:recommended', 'google'],
 	rules: {
 		'comma-dangle': 0,
-		'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+		'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
 		'max-len': 0,
 		'object-curly-spacing': 0,
 		'arrow-parens': 0,

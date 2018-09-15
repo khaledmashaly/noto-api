46aa372e5ea2a0e782ccaa0d476dc2943f97dab0
5aledmaged <5aledmaged@gmail.com>
2018-09-15 19:01:50 +0200

    added extended: false option to bodyParser.urlencoded()

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/notes', noteRouter);

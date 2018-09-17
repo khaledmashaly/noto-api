c98dee8175db206aec69d551f8b4bc24cf4a9b36
5aledmaged <5aledmaged@gmail.com>
2018-09-17 07:30:41 +0200

    add forward slash before *

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/notes', noteRouter);
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
 	res.redirect('/');
 });
 

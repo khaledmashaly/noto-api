ce746f329e175ed949cadc286a194281fcde4643
5aledmaged <5aledmaged@gmail.com>
2018-09-24 05:57:56 +0200

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,25 @@ module.exports = {
 		node: true
 	},
 	parserOptions: {
+		ecmaVersion: 2018,
 		sourceType: 'module',
 	},
-	extends: ['eslint:recommended', 'google'],
+	extends: [
+		'eslint:recommended',
+		'google',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+	],
+	settings: {
+		'import/resolver': 'node'
+	}
 	rules: {
-		'comma-dangle': 0,
-		'no-console': process.env.NODE_ENV === 'production' ? 2 : 1,
-		'max-len': 0,
-		'object-curly-spacing': 0,
-		'arrow-parens': 0,
-		'no-tabs': 0
+		'no-tabs': 'off',
+		'quote-props': ['error', 'as-needed'],
+		'comma-dangle': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+		'max-len': 'off',
+		'object-curly-spacing': 'off',
+		'arrow-parens': 'off',
 	}
 };

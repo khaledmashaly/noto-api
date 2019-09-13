fadc02d0241c6984eefd92ee9e6730f58a721373
5aledmaged <5aledmaged@gmail.com>
2019-09-13 17:15:59 +0200

    removed event listeners

--- config/db.js
+++ config/db.js
@@ -1,22 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(
-	process.env.NOTO_DB_URL,
-	{
-		useNewUrlParser: true,
-		useFindAndModify: false
-	}
-);
-
-mongoose.connection.on('connection', () => {
-	console.log('mongoose connected');
+mongoose.connect(process.env.NOTO_DB_URL, {
+	useNewUrlParser: true,
+	useFindAndModify: false
 });
-mongoose.connection.on('error', (e) => {
-	console.log('mongoose connection error:', e);
-});
-mongoose.connection.on('disconnected', () => {
-	console.log('mongoose disconnected');
-});
-
-import '../models/noteModel';
-import '../models/userModel';

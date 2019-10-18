0ebb3b39c75d75ab7715aeb5158f734b1e8aee87
5aledmaged <5aledmaged@gmail.com>
2019-10-18 13:43:58 +0200

    set useCreateIndex, to avoid mongodb warning

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@ import mongoose from 'mongoose';
 
 mongoose.connect(process.env.NOTO_DB_URL, {
 	useNewUrlParser: true,
-	useFindAndModify: false
+	useFindAndModify: false,
+	useCreateIndex: true
 });

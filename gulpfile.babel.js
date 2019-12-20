31d11ff7896c9d614eda13064a955801ad6c3a32
5aledmaged <5aledmaged@gmail.com>
2019-12-20 17:04:20 +0200

    remove copyStaticAssets gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,6 @@ import { src, dest, series } from 'gulp';
 import eslint from 'gulp-eslint';
 import babel from 'gulp-babel';
 import rimraf from 'rimraf';
-import { ncp } from 'ncp';
 
 const paths = {
 	src: ['app.js', 'routes/*.js', 'db/*.js'],
@@ -15,10 +14,6 @@ const clean = (cb) => {
 	rimraf('dist', cb);
 };
 
-const copyStaticAssets = (cb) => {
-	ncp('public', 'dist/public', cb);
-};
-
 export const buildJS = () => {
 	return src(paths.src, srcOptions)
 		.pipe(eslint())
@@ -28,6 +23,6 @@ export const buildJS = () => {
 		.pipe(dest(paths.dest));
 };
 
-const build = series(clean, buildJS, copyStaticAssets);
+const build = series(clean, buildJS);
 
 export default build;

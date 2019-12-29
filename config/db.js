7862334af474e89da317fc5aaddb20ec34ea371e
5aledmaged <5aledmaged@gmail.com>
2019-12-29 18:03:30 +0200

    use the new Server Discover engine, to remove deprecation warning

--- config/db.js
+++ config/db.js
@@ -3,5 +3,6 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.NOTO_DB_URL, {
 	useNewUrlParser: true,
 	useFindAndModify: false,
-	useCreateIndex: true
+	useCreateIndex: true,
+	useUnifiedTopology: true
 });

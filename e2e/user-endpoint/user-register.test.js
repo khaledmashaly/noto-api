b5e3338e4cb1d4d0b365ae5e896de26a5e919466
5aledmaged <5aledmaged@gmail.com>
2020-02-22 13:25:19 +0200

    added more tests for user register endpoint

--- e2e/user-endpoint/user-register.test.js
+++ e2e/user-endpoint/user-register.test.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { describe, it } from 'mocha';
 import request from 'supertest';
+import { expect } from 'chai';
 
 import { app } from '../../src/app';
 
@@ -19,22 +20,155 @@ describe('POST /user', () => {
 			.expect(201);
 	});
 
-	it('fails when email is not unique', async () => {
-		const newUser = {
-			email: 'hello2@world.com',
-			fullname: 'Hello World',
-			password: 'helloworld',
-			confirmPassword: 'helloworld'
-		};
+	describe('Email Address', () => {
+		it('must be unique', async () => {
+			const newUser = {
+				email: 'hello2@world.com',
+				fullname: 'Hello World',
+				password: 'helloworld',
+				confirmPassword: 'helloworld'
+			};
 
-		await request(app)
-			.post('/user')
-			.send(newUser)
-			.expect(201);
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(201);
 
-		await request(app)
-			.post('/user')
-			.send(newUser)
-			.expect(422);
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.error).to.equal('duplicate key');
+				});
+		});
+
+		it('must be valid', async () => {
+			const newUser = {
+				email: 'hello2world.com',
+				fullname: 'Hello World',
+				password: 'helloworld',
+				confirmPassword: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"email" must be a valid email');
+				});
+		});
+
+		it('is required', async () => {
+			const newUser = {
+				fullname: 'Hello World',
+				password: 'helloworld',
+				confirmPassword: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"email" is required');
+				});
+		});
+
+		it('shouldn\'t be empty', async () => {
+			const newUser = {
+				email: '',
+				fullname: 'Hello World',
+				password: 'helloworld',
+				confirmPassword: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"email" is not allowed to be empty');
+				});
+		});
+	});
+
+	describe('Password', () => {
+		it('is required', async () => {
+			const newUser = {
+				email: 'hello2@world.com',
+				fullname: 'Hello World',
+				confirmPassword: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"password" is required');
+				});
+		});
+	});
+
+	describe('confirmPassword', () => {
+		it('should equal password', async () => {
+			const newUser = {
+				email: 'hello2@world.com',
+				fullname: 'Hello World',
+				password: 'helloworld',
+				confirmPassword: 'helloworl'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"confirmPassword" must be [ref:password]');
+				});
+		});
+
+		it('is required', async () => {
+			const newUser = {
+				email: 'hello2@world.com',
+				fullname: 'Hello World',
+				password: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"confirmPassword" is required');
+				});
+		});
+	});
+
+	describe('fullname', () => {
+		it('is required', async () => {
+			const newUser = {
+				email: 'hello2@world.com',
+				password: 'helloworld',
+				confirmPassword: 'helloworld'
+			};
+
+			await request(app)
+				.post('/user')
+				.send(newUser)
+				.expect(422)
+				.expect((res) => {
+					expect(res.body.errors[0]).to.exist;
+					expect(res.body.errors[0].message).to.equal('"fullname" is required');
+				});
+		});
 	});
 });
